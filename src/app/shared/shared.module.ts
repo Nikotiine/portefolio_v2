7b789ab5d7e563d7cf9b842287bd3c842f8d6463
Nikotiine <niko.tiine@gmail.com>
2023-08-11 10:39:19 +0200

    footer pcard

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { FooterComponent } from './footer/footer.component';
 import { RouterLink } from '@angular/router';
 import { TranslateModule } from '@ngx-translate/core';
+import { CardModule } from 'primeng/card';
 
 @NgModule({
   declarations: [FooterComponent],
-  imports: [CommonModule, RouterLink, TranslateModule],
+  imports: [CommonModule, RouterLink, TranslateModule, CardModule],
   exports: [FooterComponent],
 })
 export class SharedModule {}

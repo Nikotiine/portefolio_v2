91417b1944d4ae2cac20f6441795fd558c6bf7c2
Nikotiine <niko.tiine@gmail.com>
2023-08-11 11:16:29 +0200

    responsive app / logo ok

---
+++ src/app/core/models/ApplicationCard.model.ts
@@ -0,0 +1,7 @@
+import { Routing } from '../enum/Routing.enum';
+
+export interface ApplicationCard {
+  title: string;
+  logo: string;
+  routerLink: Routing;
+}
--- src/app/main/applications/applications.component.ts
+++ src/app/main/applications/applications.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Routing } from '../../core/enum/Routing.enum';
 import { ApplicationService } from '../../core/services/application.service';
+import { ApplicationCard } from '../../core/models/ApplicationCard.model';
 
 @Component({
   selector: 'app-applications',
@@ -8,7 +9,48 @@ import { ApplicationService } from '../../core/services/application.service';
   styleUrls: ['./applications.component.scss'],
 })
 export class ApplicationsComponent {
-  protected readonly Routing = Routing;
+  public applications: ApplicationCard[] = [
+    {
+      logo: 'who.png',
+      routerLink: Routing.HOME,
+      title: 'whoIAm',
+    },
+    {
+      logo: 'knowledge.png',
+      routerLink: Routing.KNOWLEDGE,
+      title: 'knowledge',
+    },
+    {
+      logo: 'curriculum.png',
+      routerLink: Routing.CURRICULUM,
+      title: 'cv',
+    },
+    {
+      logo: 'project.png',
+      routerLink: Routing.PROJECTS,
+      title: 'projects',
+    },
+    {
+      logo: 'social.png',
+      routerLink: Routing.SOCIAL_NETWORK,
+      title: 'socialNetwork',
+    },
+    {
+      logo: 'contact.png',
+      routerLink: Routing.SOCIAL_NETWORK,
+      title: 'contactMe',
+    },
+    {
+      logo: 'tuto.png',
+      routerLink: Routing.SOCIAL_NETWORK,
+      title: 'tutorial',
+    },
+    {
+      logo: 'setting.png',
+      routerLink: Routing.SOCIAL_NETWORK,
+      title: 'setting',
+    },
+  ];
   constructor(private readonly applicationService: ApplicationService) {}
   public closeSideBar(): void {
     this.applicationService.sidebarVisible$.next(false);

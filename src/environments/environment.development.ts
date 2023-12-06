301c68b4ef97b41ed50457c381f207a95c6b67de
Nikotiine <92221729+Nikotiine@users.noreply.github.com>
2023-12-06 19:52:50 +0100

    Delete src/environments directory

--- src/environments/environment.development.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: false,
-  recaptcha: {
-    siteKey: '6LfqEycpAAAAAPC-gbomHEGVCwasSfq9__QhlS1I',
-  },
-};
--- src/environments/environment.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  recaptcha: {
-    siteKey: '6LfqEycpAAAAAPC-gbomHEGVCwasSfq9__QhlS1I',
-  },
-};
